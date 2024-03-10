eb933c8866c004f423f1e915d599f4a860d3c95b
ericgarciafernandez <ericgarciafernandez18@gmail.com>
2024-03-11 00:43:46 +0100

    Add environment and fetch with API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [productos, setProductos] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_SERVER}/productos`)
+      .then((result) => result.json())
+      .then((value) => setProductos(value));
+  }, []);
   return (
-    <div className="App">
-      Api productos
+    <div className="text-2xl font-bold text-center text-green-600">
+      Test api productos
+      <div>
+        <p>Productos: </p>
+        {
+          productos.map((el) => el.name)
+        }
+      </div>
     </div>
   );
 }
